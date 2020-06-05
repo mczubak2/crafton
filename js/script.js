6a91c64f5988eb430ae100700f5aa2af8c9c2773
Mikolaj Czubak <mczubak2@edu.cdv.pl>
2020-06-06 00:25:41 +0200

    mid

--- js/script.js
+++ js/script.js
@@ -7,3 +7,27 @@ circle.forEach(element => {
         element.classList.add('active');
     });
 });
+
+function initMap() {
+    let uluru = {
+        lat: 52.403190,
+        lng: 16.910160
+    };
+    let map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 16,
+            center: uluru
+        });
+}
+
+function toggle() {
+    const blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    const popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
+
+window.addEventListener('scroll', () => {
+    let nav = document.querySelector('.navigation');
+    nav.classList.toggle('sticky', window.scrollY > 0);
+})
